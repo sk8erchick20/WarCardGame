0ed9d312f5102a373e0ac83cd25630b4f60a4d2e
Karishma <tikkysood@yahoo.com>
2015-01-19 22:41:01 -0700

    Update PlayingCard.java

--- src/PlayingCard.java
+++ src/PlayingCard.java
@@ -1,33 +1,19 @@
-public abstract class PlayingCard 
+public abstract class PlayingCard
 {
- 
- // represents the suit of the card as an integer
- //    Heart, Club, Diamond or Spade
- //   = 1      2     3          4
- private int suit;
- 
- // represents the rank of the card as an integer
- //    Ace 2 3 4 5 6 7 8 9 10 Jack Queen King
- //   =  1 2 3 4 5 6 7 8 9 10 11   12    13
- private int rank;
- 
- public PlayingCard(int cardSuit, int cardRank) 
- {
-  suit=cardSuit;
-  rank=cardRank;
- }
-  
- public abstract String toString();
- 
- // returns the suit of this card
- public int getSuit() 
- {
-  return suit;
- }
- 
- // returns the rank of this card
- public int getRank() 
- {
-  return rank;
- }
+  private int suit;
+  private int rank;
+  public PlayingCard(int cardSuit, int cardRank)
+  {
+    suit=cardSuit;
+    rank=cardRank;
+  }
+  public abstract String toString();
+  public int getSuit()
+  {
+    return suit;
+  }
+  public int getRank()
+  {
+    return rank;
+  }
 }
